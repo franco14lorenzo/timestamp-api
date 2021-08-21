ac9c56ef9bec464e9d54692f7e3d9a6aa090ecf0
franco14lorenzo <franco14lorenzo@gmail.com>
2021-08-21 16:08:13 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+# TIMESTAMP MICROSERVICE
 
-# [Timestamp Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice)
+Este es el proyecto de un microservicio que sirve fechas, forma parte del primero de cinco proyectos requeridos por freecodecamp.org para obtener el certificado de APIs y microservicios.
+
+https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
+
+DEMO:
+
+![Timestamp Microservice](https://i.ibb.co/Ttzt80R/timestamp-microservice.png)
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const { response } = require('express');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -18,15 +19,50 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+/* app.get('/api/:timestamp', (req, res) => {
+  let timestamp = req.params.timestamp
+  if (timestamp.match(/\d{5,}/)){
+    timestamp = +timestamp
+    let unix = timestamp
+    let utc = new Date(timestamp).toUTCString()
+    if (utc === "Invalid Date"){
+      res.json({"error": "Invalid Date"})
+    }else{
+      res.json({"unix": unix, "utc": utc})
+    }
+  } else{
+    let unix = new Date(timestamp).valueOf()
+    let utc = new Date(timestamp).toUTCString()
+    if (utc === "Invalid Date"){
+      res.json({"error": "Invalid Date"})
+    }else{
+      res.json({"unix": unix, "utc": utc})
+    }
+  }
+}) */
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get('/api/:timestamp', (req, res) => {
+  let timestamp = req.params.timestamp
+  if (timestamp.match(/\d{5,}/)){
+    timestamp = +timestamp
+  }
+    let date = new Date(timestamp)
+    if(date.toUTCString() == "Invalid Date"){
+      res.json({"error": date.toUTCString()})
+    }
+    res.json({"unix": date.valueOf(), "utc": date.toUTCString()})
+})
 
+app.get('/api/', (req, res) => {
+  
+  res.json({
+    "unix": new Date().valueOf(),
+    "utc": new Date().toUTCString()
+  })
+})
 
+// listen for requests :) 
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
